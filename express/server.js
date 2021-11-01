0019a56edb1a610a395eee09c716a83a26f755dc
da-the-dev <leshiktk@me.com>
2021-11-01 17:09:59 +0300

    Now can deployed

--- express/server.js
+++ express/server.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const v1 = require('./api/v1')
 const v2 = require('./api/v2')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.sendStatus(200)
+    res.status(200).send('Hi! You are on a backend server!')
 })
 
 app.use('/api/v1', v1)
 app.use('/api/v2', v2)
 
 app.listen(8080, () => {
-    console.log('Server started! ')
+    console.log('Server started!')
 })
